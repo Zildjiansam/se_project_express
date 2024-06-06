bb3c7d31a8e786f4b69b6b7f3c39c99af966f1ab
Ian McDonald <ISMcDonald85@gmail.com>
2024-06-05 23:40:27 -0500

    Fix controllers/routesIndex/clothingItemsMod for 1st review

--- routes/routesIndex.js
+++ routes/routesIndex.js
@@ -1,15 +1,14 @@
-const { SERVER_ERROR } = require("../utils/errorCodes");
-
 const router = require("express").Router();
 const userRouter = require("./usersRouters");
 const clothingItemsRouter = require("./clothingItemsRouters");
+const { SERVER_ERROR } = require("../utils/errorCodes");
 
 router.use("/users", userRouter);
 
 router.use("/items", clothingItemsRouter);
 
-router.use((req, res) => {
-  return res.status(SERVER_ERROR).send({ message: "Resource not found" });
-});
+router.use((req, res) =>
+  res.status(SERVER_ERROR).send({ message: "Resource not found" })
+);
 
 module.exports = router;
